2018098892956ab1c2f599c32e2c6359bb9d70ae
AndrewPal04 <aapalacios@cpp.edu>
2025-05-12 19:37:55 -0700

    Esp logic tictactoe.c
    Added logic from esp32 to handle wins

--- tictactoe.c
+++ tictactoe.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <windows.h>
 
-#define BROKER_IP "34.53.29.173"  // change here
+#define BROKER_IP "34.53.123.240"  // change here
 
-char board[9] = {'1','2','3','4','5','6','7','8','9'};
+char board[10];
 
 void drawBoard() {
     system("cls || clear");
@@ -59,122 +59,210 @@ char checkWin() {
 }
 
 void runMode1() {
-    int userMove, pos;
-    char symbol, result;
+    int userMove;
+    char result = ' ';
+    char boardResponse[64];
+    char boardCopy[10];
 
-    resetBoard();
+    const char* broker_ip = "34.53.123.240";
+    FILE *fp;
+    {
+        char subInit[512];
+        sprintf(subInit, "mosquitto_sub -h %s -t game/board -C 1", broker_ip);
+        fp = popen(subInit, "r");
+        if (fp && fgets(boardResponse, sizeof(boardResponse), fp)) {
+            boardResponse[strcspn(boardResponse, "\r\n")] = 0;
+            // parse
+            char* t = strtok(boardResponse, ",");
+            if (t) {
+                strncpy(boardCopy, t, 9);
+                boardCopy[9] = '\0';
+                t = strtok(NULL, ",");
+                result = (t && strlen(t) > 0) ? toupper(t[0]) : ' ';
+            }
+            memcpy(board, boardCopy, 9);
+        }
+        if (fp) pclose(fp);
+    }
+
+    drawBoard();
 
     while (1) {
-        drawBoard();
+        if (result == 'X') { printf("You win!\n"); break; }
+        if (result == 'O') { printf("You lost.\n"); break; }
+        if (result == 'T') { printf("It's a draw!\n"); break; }
 
         printf("Your Move (1-9): ");
         scanf("%d", &userMove);
+        getchar();
 
-        if (userMove < 1 || userMove > 9 || !isMoveValid(userMove - 1)) {
-            printf("Invalid move. Try again.\n");
-            getchar();  // clear leftover newline
-            continue;
+        {
+            char pubCmd[256];
+            sprintf(pubCmd, "mqtt_send.bat %d", userMove);
+            system(pubCmd);
         }
 
-        makeMove(userMove - 1, 'X');
-        result = checkWin();
-        if (result == 'X') {
-            drawBoard();
-            printf("You win!\n");
-            break;
-        } else if (result == 'T') {
-            drawBoard();
-            printf("It's a draw!\n");
-            break;
+        {
+            char sub1[512];
+            sprintf(sub1, "mosquitto_sub -h %s -t game/board -C 1", broker_ip);
+            fp = popen(sub1, "r");
         }
-
-        // 🔁 Step 1: Send local board to GCP
-        char boardString[10];
-        for (int i = 0; i < 9; i++) boardString[i] = board[i];
-        boardString[9] = '\0';
-
-        char updateCmd[512];
-        sprintf(updateCmd,
-            "ssh -i C:/Users/Andre/.ssh/id_ed25519 andrewpalacios2004@%s \"echo '%s' > /home/andrewpalacios2004/Project3/board.txt\"",
-            BROKER_IP, boardString);
-        system(updateCmd);
-
-        // 🔁 Step 2: Run playervsbash.sh remotely
-        char sshCmd[512];
-        sprintf(sshCmd,
-            "ssh -i C:/Users/Andre/.ssh/id_ed25519 andrewpalacios2004@%s \"cd /home/andrewpalacios2004/Project3 && ./playervsbash.sh\"",
-            BROKER_IP);
-
-        FILE *fp = popen(sshCmd, "r");
-        if (!fp) {
-            perror("Failed to SSH and run playervsbash.sh");
-            return;
-        }
-
-        char line[32];
-        if (fgets(line, sizeof(line), fp) == NULL) {
-            perror("No move received from GCP");
-            pclose(fp);
+        if (!fp || fgets(boardResponse, sizeof(boardResponse), fp) == NULL) {
+            perror("read failed");
+            if (fp) pclose(fp);
             return;
         }
         pclose(fp);
 
-        // Expecting format: "O 5"
-        if (sscanf(line, "%c %d", &symbol, &pos) != 2 || pos < 1 || pos > 9) {
-            printf("Failed to parse opponent move: %s\n", line);
-            break;
-        }
+        boardResponse[strcspn(boardResponse, "\r\n")] = 0;
 
-        if (!isMoveValid(pos - 1)) {
-            printf("Opponent made invalid move: %d\n", pos);
-            break;
+        {
+            char* t = strtok(boardResponse, ",");
+            if (t) {
+                strncpy(boardCopy, t, 9);
+                boardCopy[9] = '\0';
+                t = strtok(NULL, ",");
+                result = (t && strlen(t) > 0) ? toupper(t[0]) : ' ';
+            }
         }
-
-        makeMove(pos - 1, symbol);
+        memcpy(board, boardCopy, 9);
         drawBoard();
 
-        result = checkWin();
-        if (result == 'O') {
-            printf("You lost.\n");
-            break;
-        } else if (result == 'T') {
-            printf("It's a draw.\n");
-            break;
+        if (result == 'X') { printf("You win!\n"); break; }
+        if (result == 'O') { printf("You lost.\n"); break; }
+        if (result == 'T') { printf("It's a draw!\n"); break; }
+
+        printf("Opponent's turn...\n");
+        {
+            char repub[512];
+            sprintf(repub, "mosquitto_pub -h %s -t game/board -m \"%.9s,%c\"", broker_ip, board, result);
+            system(repub);
+            char sshCmd[512];
+            sprintf(sshCmd,
+                "ssh -i C:/Users/Andre/.ssh/id_ed25519 andrewpalacios2004@%s '/home/andrewpalacios2004/Project3/player2.sh'",
+                broker_ip);
+            system(sshCmd);
         }
+
+        {
+            char sub2[512];
+            sprintf(sub2, "mosquitto_sub -h %s -t game/board -C 1", broker_ip);
+            fp = popen(sub2, "r");
+        }
+        if (!fp || fgets(boardResponse, sizeof(boardResponse), fp) == NULL) {
+            perror("read bot failed");
+            if (fp) pclose(fp);
+            return;
+        }
+        pclose(fp);
+
+        boardResponse[strcspn(boardResponse, "\r\n")] = 0;
+
+        {
+            char* t = strtok(boardResponse, ",");
+            if (t) {
+                strncpy(boardCopy, t, 9);
+                boardCopy[9] = '\0';
+                t = strtok(NULL, ",");
+                result = (t && strlen(t) > 0) ? toupper(t[0]) : ' ';
+            }
+        }
+        memcpy(board, boardCopy, 9);
+        drawBoard();
+
+        if (result == 'X') { printf("You win!\n"); break; }
+        if (result == 'O') { printf("You lost.\n"); break; }
+        if (result == 'T') { printf("It's a draw!\n"); break; }
     }
 
+    Sleep(2000);
     printf("Game over. Press Enter to return to menu...");
-    getchar(); getchar();
+    while (getchar()!='\n');
 }
 
-
 void runMode2() {
-    int currentPlayer = 0;
     int move;
-    char symbols[2] = {'X', 'O'};
+    char result = ' ';
+    char boardResponse[64];
+    char boardCopy[10];
+    const char* broker_ip = "34.53.123.240";
+    const char* mqtt_path = "C:\\mosq\\mosquitto_pub.exe";
 
-    resetBoard();
+    int currentPlayer = 0;
+    const char* topics[2] = {"player1", "player2"};
+    const char symbols[2] = {'X', 'O'};
+
+    char subInit[512];
+    sprintf(subInit, "mosquitto_sub -h %s -t game/board -C 1", broker_ip);
+    FILE* fp = popen(subInit, "r");
+    if (fp && fgets(boardResponse, sizeof(boardResponse), fp)) {
+        char* token = strtok(boardResponse, ",");
+        if (token) {
+            strncpy(boardCopy, token, 9);
+            boardCopy[9] = '\0';
+            token = strtok(NULL, ",");
+            result = (token && strlen(token) > 0) ? toupper(token[0]) : ' ';
+        }
+        memcpy(board, boardCopy, 9);
+    }
+    if (fp) pclose(fp);
+
+    drawBoard();
 
     while (1) {
-        drawBoard();
+        if (result == 'X') {
+            printf("Player X wins!\n");
+            break;
+        } else if (result == 'O') {
+            printf("Player O wins!\n");
+            break;
+        } else if (result == 'T') {
+            printf("It's a draw!\n");
+            break;
+        }
+
         printf("Player %c, enter your move (1-9): ", symbols[currentPlayer]);
         scanf("%d", &move);
+        getchar();
 
-        if (move < 1 || move > 9 || !isMoveValid(move - 1)) {
+        if (move < 1 || move > 9) {
             printf("Invalid move. Try again.\n");
-            getchar(); getchar();
             continue;
         }
 
-        makeMove(move - 1, symbols[currentPlayer]);
+        char pubCmd[256];
+        sprintf(pubCmd, "mosquitto_pub -h %s -t game/move/%s -m \"%d\"", broker_ip, topics[currentPlayer], move);
+        system(pubCmd);
 
-        char result = checkWin();
-        if (result == 'X' || result == 'O') {
-            drawBoard();
-            printf("Player %c wins!\n", result);
+        char subCmd[512];
+        sprintf(subCmd, "mosquitto_sub -h %s -t game/board -C 1", broker_ip);
+        fp = popen(subCmd, "r");
+        if (!fp || fgets(boardResponse, sizeof(boardResponse), fp) == NULL) {
+            perror("Failed to receive board from ESP32");
+            if (fp) pclose(fp);
+            return;
+        }
+        pclose(fp);
+
+        boardResponse[strcspn(boardResponse, "\r\n")] = 0;
+        char* token = strtok(boardResponse, ",");
+        if (token) {
+            strncpy(boardCopy, token, 9);
+            boardCopy[9] = '\0';
+            token = strtok(NULL, ",");
+            result = (token && strlen(token) > 0) ? toupper(token[0]) : ' ';
+        }
+
+        memcpy(board, boardCopy, 9);
+        drawBoard();
+
+        if (result == 'X') {
+            printf("Player X wins!\n");
+            break;
+        } else if (result == 'O') {
+            printf("Player O wins!\n");
             break;
         } else if (result == 'T') {
-            drawBoard();
             printf("It's a draw!\n");
             break;
         }
@@ -182,44 +270,149 @@ void runMode2() {
         currentPlayer = 1 - currentPlayer;
     }
 
-    printf("Press Enter to return to menu...");
-    getchar();
+    Sleep(2000);
+    printf("Game over. Press Enter to return to menu...");
+    while (getchar() != '\n');
 }
+
+
 void runMode3() {
-    resetBoard();
-    drawBoard();
+    char boardResponse[64];
+    char boardCopy[10];
+    char result = ' ';
+    const char* broker_ip = "34.53.123.240";
+    const char* mqtt_path = "C:\\mosq\\mosquitto_pub.exe";
 
-    char sshCmd[256];
-    sprintf(sshCmd,
-        "ssh -i C:/Users/Andre/.ssh/id_ed25519 andrewpalacios2004@%s \"cd /home/andrewpalacios2004/Project3 && ./game.sh\"",
-        BROKER_IP);
+    char resetCmd[256];
+    sprintf(resetCmd, "%s -r -h %s -t game/board -m \"123456789, \"", mqtt_path, broker_ip);
+    system(resetCmd);
+    Sleep(1000);
 
-    FILE *fp = popen(sshCmd, "r");
-    if (!fp) {
-        perror("SSH failed");
-        return;
-    }
+    system("player1.exe");
+    Sleep(1000);
 
-    char line[128];
-    char symbol;
-    int pos;
+    while (1) {
 
-    while (fgets(line, sizeof(line), fp)) {
-        if (sscanf(line, "%c %d", &symbol, &pos) == 2) {
-            if (pos >= 1 && pos <= 9) {
-                makeMove(pos - 1, symbol);
-                drawBoard();
-                Sleep(1000);
+        int attempts = 0;
+        do {
+            FILE *fp = popen("mosquitto_sub -h 34.53.123.240 -t game/board -C 1", "r");
+            if (!fp || fgets(boardResponse, sizeof(boardResponse), fp) == NULL) {
+                perror("Failed to read board");
+                if (fp) pclose(fp);
+                return;
             }
-        } else {
-            printf("%s", line);
+            pclose(fp);
+
+            boardResponse[strcspn(boardResponse, "\r\n")] = 0;
+
+            char* token = strtok(boardResponse, ",");
+            if (token) {
+                strncpy(boardCopy, token, 9);
+                boardCopy[9] = '\0';
+                token = strtok(NULL, ",");
+                result = (token && strlen(token) > 0) ? toupper(token[0]) : ' ';
+                if (result != 'X' && result != 'O' && result != 'T') result = ' ';
+            } else {
+                result = ' ';
+            }
+
+            attempts++;
+            if (attempts > 10) {
+                printf("Timeout: no valid board received after 10 attempts.\n");
+                return;
+            }
+
+            Sleep(500);
+        } while (strcmp(boardCopy, "123456789") == 0);
+
+        memcpy(board, boardCopy, 9);
+        drawBoard();
+
+        if (result == 'X') {
+            printf("Player 1 (C) wins!\n");
+            break;
+        } else if (result == 'O') {
+            printf("Player 2 (Bash) wins!\n");
+            break;
+        } else if (result == 'T') {
+            printf("It's a draw!\n");
+            break;
         }
+
+        int countX = 0, countO = 0;
+        for (int i = 0; i < 9; i++) {
+            if (board[i] == 'X') countX++;
+            else if (board[i] == 'O') countO++;
+        }
+
+        if (countX <= countO) {
+            Sleep(1000);
+            system("player1.exe");
+        } else {
+            Sleep(1000);
+            char sshCmd[512];
+            sprintf(sshCmd,
+                "ssh -i C:/Users/Andre/.ssh/id_ed25519 andrewpalacios2004@%s '/home/andrewpalacios2004/Project3/player2.sh'",
+                broker_ip);
+            system(sshCmd);
+        }
+
+        int moveAttempts = 0;
+        do {
+            FILE *fp = popen("mosquitto_sub -h 34.53.123.240 -t game/board -C 1", "r");
+            if (!fp || fgets(boardResponse, sizeof(boardResponse), fp) == NULL) {
+                perror("Failed to read post-move board");
+                if (fp) pclose(fp);
+                return;
+            }
+            pclose(fp);
+
+            boardResponse[strcspn(boardResponse, "\r\n")] = 0;
+            char* token = strtok(boardResponse, ",");
+            if (token) {
+                strncpy(boardCopy, token, 9);
+                boardCopy[9] = '\0';
+                token = strtok(NULL, ",");
+                result = (token && strlen(token) > 0) ? toupper(token[0]) : ' ';
+                if (result != 'X' && result != 'O' && result != 'T') result = ' ';
+            } else {
+                result = ' ';
+            }
+
+            moveAttempts++;
+            if (moveAttempts > 10) {
+                printf("Timeout waiting for board after move.\n");
+                return;
+            }
+
+            Sleep(500);
+        } while (strcmp(boardCopy, "123456789") == 0);
+
+        memcpy(board, boardCopy, 9);
+        drawBoard();
+
+        if (result == 'X') {
+            printf("Player 1 (C) wins!\n");
+            break;
+        } else if (result == 'O') {
+            printf("Player 2 (Bash) wins!\n");
+            break;
+        } else if (result == 'T') {
+            printf("It's a draw!\n");
+            break;
+        }
+
+        Sleep(1000);
     }
 
-    pclose(fp);
+    Sleep(3000);
+    int ch;
     printf("Game complete. Press Enter to return to menu...");
-    getchar(); getchar();
+    while ((ch = getchar()) != '\n' && ch != EOF);
+    getchar();
+
 }
+
 
 
 void showMenu() {
@@ -228,9 +421,9 @@ void showMenu() {
         system("clear");
         printf("Welcome to ESP32 Tic-Tac-Toe\n");
         printf("-----------------------------\n");
-        printf("1. Laptop vs Bash\n");
-        printf("2. Laptop vs Laptop\n");
-        printf("3. Auto Play\n");
+        printf("1. Laptop vs Bash (1 player)\n");
+        printf("2. Laptop vs Laptop (2 player)\n");
+        printf("3. Auto Play (C vs Bash)\n");
         printf("4. Exit\n");
         printf("\nSelect a mode: ");
         scanf("%d", &choice);
@@ -239,7 +432,6 @@ void showMenu() {
             runMode1();
         } else if (choice == 2) {
             runMode2();
-            getchar();
         } else if (choice == 3) {
             runMode3();
         } else if (choice == 4) {
